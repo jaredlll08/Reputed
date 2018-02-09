d41ccf679bf5099c1bb03bb0b6d590a0bf6227bc
Jared <jaredlll08@gmail.com>
2018-02-09 19:48:01 +0200

    Fixed explosions counting as kills

--- src/main/java/com/blamejared/reputed/events/CommonEventHandler.java
+++ src/main/java/com/blamejared/reputed/events/CommonEventHandler.java
@@ -3,24 +3,31 @@ package com.blamejared.reputed.events;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.init.Items;
-import net.minecraft.inventory.*;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.*;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.AnvilUpdateEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.event.entity.player.*;
-import net.minecraftforge.fml.common.eventhandler.*;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Collection;
 
 public class CommonEventHandler {
     
     @SubscribeEvent
-    public void onLivingDeath(LivingDeathEvent event) {
-        Entity source = event.getSource().getTrueSource();
-        if(source instanceof EntityLivingBase) {
+    public void onLivingAttack(LivingAttackEvent event) {
+        DamageSource source1 = event.getSource();
+        Entity source = source1.getTrueSource();
+        String type = source1.getDamageType();
+        if(source instanceof EntityLivingBase && type.equalsIgnoreCase("player") || type.equalsIgnoreCase("arrow")) {
             EntityLivingBase ent = (EntityLivingBase) source;
             ItemStack stack = ent.getHeldItemMainhand();
+            if(event.getEntityLiving().getHealth() - event.getAmount() > 0) {
+                return;
+            }
             if(!stack.isEmpty()) {
                 NBTTagCompound tag = stack.getTagCompound();
                 if(tag != null && tag.hasKey("reputed")) {
@@ -52,7 +59,7 @@ public class CommonEventHandler {
             nbt.setBoolean("reputed", true);
             nbt.setInteger("kills", 0);
             output.setTagCompound(nbt);
-            output.setStackDisplayName("Reputed " + output.getDisplayName());
+            output.setStackDisplayName(TextFormatting.RESET + "Reputed " + output.getDisplayName());
             event.setOutput(output);
             event.setCost(1);
         }
